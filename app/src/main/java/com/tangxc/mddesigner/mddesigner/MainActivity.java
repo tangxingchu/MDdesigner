39df6407c1f1c545022b2af475d67056f0b1cb60
tangxingchu <51697550@qq.com>
2017-08-02 17:49:01 +0800

    提交

--- app/src/main/java/com/tangxc/mddesigner/mddesigner/MainActivity.java
+++ app/src/main/java/com/tangxc/mddesigner/mddesigner/MainActivity.java
@@ -3,6 +3,9 @@ package com.tangxc.mddesigner.mddesigner;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,14 +15,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setLogo(R.drawable.ic_launcher);
+        //toolbar.setLogo(R.drawable.ic_launcher);
         setSupportActionBar(toolbar);
-
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
         /*
         setContentView(R.layout.activity_main_back);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -48,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
             case R.id.launcher:
                 Toast.makeText(this, "launcher", Toast.LENGTH_SHORT).show();
                 break;
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
             default: break;
         }
         return true;
